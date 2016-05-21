340a06d9746667fbeb76b5593a814f9889b92993
Claudio Vallejo <hola@claudiovallejo.mx>
2016-05-20 23:05:06 -0500

    Fiddled a bit with the code.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 //  Store a reference to all Student Profiles
-var studentList = document.getElementsByClassName('student-item');
-console.log(studentList.length);
+var studentProfilesList = document.getElementsByClassName('student-item');
+console.log(studentProfilesList.length);
+
+var studentProfilePageLimit = 10;
+var test2 = (Math.round(studentProfilesList.length / studentProfilePageLimit) + 1 ) * studentProfilePageLimit;
+
+console.log(test2);
+for (var index = 0; index <= test2; index++) {
+  window["page"+(index + 1)] = [];
+}
