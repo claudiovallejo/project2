d8169164de9bc2dafe0e344eb1d6ac50ecaa73dd
Claudio Vallejo <hola@claudiovallejo.mx>
2016-06-03 18:44:48 -0500

    Added button interactivity; Encapsulated dynamic profile display into a function.

--- js/script.js
+++ js/script.js
@@ -10,47 +10,30 @@ var $studentList = document.getElementsByClassName('student-list')[0];
 var numberOfProfiles = $studentList.children.length;
 var profilePageLimit = 10;
 var totalPagesNeeded = Math.ceil(numberOfProfiles / profilePageLimit);
-
 var testArray = [];
 for (var index = 0; index < numberOfProfiles; index++) {
   testArray.push($studentList.children[index]);
 }
-
-$studentList.innerHTML = "";
-
-// Update Student List - - - - - - - - - - - - -
-for (var index = 0; index < profilePageLimit; index++) {
-  $studentList.appendChild(testArray[index]);
-  console.log("Student Profile #" + index + " has been added to the DOM.");
-}
-
 //  Add Pagination - - - - - - - - - - - - -
 var $pagination = document.createElement('div');
 $pagination.className = "pagination";
 var $paginationList = document.createElement('ul');
 $pagination.appendChild($paginationList);
-
 for (var index = 1; index <= totalPagesNeeded; index++) {
   var $paginationItem = document.createElement('li');
   var $paginationLink = document.createElement('a');
-
   $paginationItem.appendChild($paginationLink);
-
   if (index == 1) {
     $paginationLink.className = 'active';
   }
-
   $paginationLink.setAttribute('href', '#');
   $paginationLink.innerHTML = index;
-
   //  Add Event Listener to Link
   $paginationLink.addEventListener('click', function(){
     updateList(this);
   });
-
   $paginationList.appendChild($paginationItem);
 }
-
 $page.appendChild($pagination);
 
 //  Reset Buttons
@@ -58,42 +41,50 @@ function removeActiveClass() {
   var $activeButton = document.getElementsByClassName('active')[0];
   $activeButton.classList = "";
 }
-
 //  Update List Function
 function updateList($button) {
   var pageIndex = Number($button.innerHTML);
+  var indexA = ((pageIndex - 1) * profilePageLimit);
+  var indexB = pageIndex * profilePageLimit;
+  if (testArray.length < indexB) {
+    indexB = testArray.length;
+  }
+  //  Remove class from Active button; Add Class to Clicked button
   removeActiveClass();
   $button.classList = "active";
-  //  THIS IS WHERE I LEFT OFF!
+  //  Clear Student List; Fill in List with requested Profiles
+  $studentList.innerHTML = "";
+  for (var index = indexA; index < indexB; index++) {
+    $studentList.appendChild(testArray[index]);
+    // console.log("Profile #" + index + " has been added to the DOM.");
+  }
 }
-
 //  Add Search Field - - - - - - - - - - - -
 var $pageHeader = document.getElementsByClassName('page-header')[0];
 //  Create Student Search Div; Set Class to `student-search`
 var $studentSearch = document.createElement('div');
 $studentSearch.className = 'student-search';
 $pageHeader.appendChild($studentSearch);
-
 //  Create Input Field; set placeholder attribute to 'Search for students...'
 var $input = document.createElement('input');
 $input.setAttribute('placeholder', 'Search for students...');
-
 //  Create pseudo div; Set pseudo attribute; Set style attribute; append pseudo div to input
 var $pseudo = document.createElement('div');
 $pseudo.setAttribute('pseudo', '-webkit-input-placeholder');
 $pseudo.setAttribute('style', 'display: block !important; text-overflow: clip;');
 $pseudo.innerHTML = 'Search for students...';
 $input.appendChild($pseudo);
-
 //  Create contenteditable div; set contenteditable attribute; append contentEditable div to input
 var $contentEditable = document.createElement('div');
 $contentEditable.setAttribute('contenteditable', 'plaintext-only');
 $input.appendChild($contentEditable);
-
 //  Create Button Element
 var $button = document.createElement('button');
 $button.innerHTML = 'Search';
-
 //  Append Input Field and Button to Student Search Div
 $studentSearch.appendChild($input);
 $studentSearch.appendChild($button);
+//  Update Student List
+$studentList.innerHTML = "";
+var $defaultList = document.getElementsByClassName('active')[0];
+updateList($defaultList);
